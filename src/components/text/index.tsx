113514079b2778756a7838ef3bafe0133e7d49d0
Hugo Wallenburg <hugo@hugow.no>
2023-11-21 19:13:12 +0100

    Adds fonts through fontsource

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -7,8 +7,8 @@ import { FCWithChildren } from "../../../types/utils_types"
 const HEADER_CLASSES = [
   "font-['Twinkle_Star'] text-2xl tracking-[0.03475rem]",
   "font-['Twinkle_Star'] text-xl tracking-[0.03375rem]",
-  "font-[Nunito] text-lg font-bold",
-  "font-[Nunito] text-lg"
+  "font-['Nunito_Variable'] text-lg font-bold",
+  "font-['Nunito_Variable'] text-lg"
 ] as const
 
 type HeaderComponentProps = HTMLAttributes<HTMLHeadingElement> & {
