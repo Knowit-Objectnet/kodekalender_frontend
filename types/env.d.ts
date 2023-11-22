bb0f521d94e37d2544757b92f44f236fcd8391e6
Hugo Wallenburg <hugo@hugow.no>
2023-11-22 17:19:33 +0100

    Disables light mode toggle

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 interface ImportMeta {
   env: {
-    VITE_BACKEND_HOST?: string
+    VITE_BACKEND_HOST: string
+    VITE_ENABLE_LIGHT_MODE: string
   }
 }
