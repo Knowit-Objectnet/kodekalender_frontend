1a24228ee1b588942c3b7e9bd2856afcf6210643
Hugo Wallenburg <hugo@hugow.no>
2023-11-23 02:27:23 +0100

    Pushes countdown hooks deeper to prevent some unnecessary redraws

--- src/pages/Countdown.tsx
+++ src/pages/Countdown.tsx
@@ -1,5 +1,5 @@
 import { toInteger } from "lodash-es"
-import { FC, memo } from "react"
+import { FC, memo, useMemo } from "react"
 
 import { Header1, Header3 } from "../components/text"
 import useCountdownParts from "../hooks/useCountdownParts"
@@ -9,15 +9,16 @@ import RegisterButton from "../components/RegisterButton"
 
 
 type TimerDisplayProps = {
-  t: number
+  countdownPartIndex: 0 | 1 | 2 | 3
   className?: string
 }
 
-const TimerDisplay: FC<TimerDisplayProps> = memo(({ t, className }) => {
+const TimerDisplay: FC<TimerDisplayProps> = memo(({ countdownPartIndex, className }) => {
+  const t = useCountdownParts()[countdownPartIndex]
   const tens = toInteger(t / 10)
   const ones = t % 10
 
-  return (
+  const content = useMemo(() => (
     <div
       className={cl(
         `
@@ -35,20 +36,18 @@ const TimerDisplay: FC<TimerDisplayProps> = memo(({ t, className }) => {
       <Header3 as="span" className="text-xl">{tens}</Header3>
       <Header3 as="span" className="text-xl">{ones}</Header3>
     </div>
-  )
+  ), [tens, ones, className])
+
+  return content
 })
 
 const Separator = () => (
   <Header3 as="span" className="text-xl mx-3">:</Header3>
 )
 
-const Countdown = () => {
-  const [days, hours, minutes, seconds] = useCountdownParts()
-
-
-  return (
-    <PageContent
-      className={`
+const Countdown = () => (
+  <PageContent
+    className={`
         grid
         grid-flow-row
         gap-24
@@ -56,11 +55,11 @@ const Countdown = () => {
         content-center
         justify-items-center
       `}
-    >
-      <Header1 className="inline-block leading-none">Kodekalender {getActiveYear()}</Header1>
+  >
+    <Header1 className="inline-block leading-none">Kodekalender {getActiveYear()}</Header1>
 
-      <div
-        className={`
+    <div
+      className={`
           m-auto
 
           inline-grid
@@ -71,37 +70,36 @@ const Countdown = () => {
           text-center
           child:self-center
         `}
-        title={`Første luke åpnes ${dateFormat(getRaffleStart(), "long")}.`}
-      >
-        <Header3 as="span">Dager</Header3>
-        <span />
-        <Header3 as="span">Timer</Header3>
-        <span />
-        <Header3 as="span">Minutter</Header3>
-        <span />
-        <Header3 as="span">Sekunder</Header3>
+      title={`Første luke åpnes ${dateFormat(getRaffleStart(), "long")}.`}
+    >
+      <Header3 as="span">Dager</Header3>
+      <span />
+      <Header3 as="span">Timer</Header3>
+      <span />
+      <Header3 as="span">Minutter</Header3>
+      <span />
+      <Header3 as="span">Sekunder</Header3>
 
-        <TimerDisplay t={days} />
-        <Separator />
-        <TimerDisplay t={hours} />
-        <Separator />
-        <TimerDisplay t={minutes} />
-        <Separator />
-        <TimerDisplay t={seconds} />
-      </div>
+      <TimerDisplay countdownPartIndex={0} />
+      <Separator />
+      <TimerDisplay countdownPartIndex={1} />
+      <Separator />
+      <TimerDisplay countdownPartIndex={2} />
+      <Separator />
+      <TimerDisplay countdownPartIndex={3} />
+    </div>
 
-      <p className="w-304">
-        Bli med på Knowits julekalender! Hver dag før jul åpnes en ny luke med
-        en oppgave du må løse. Vi trekker en heldig vinner som får en valgfri
-        mobil eller nettbrett!
-      </p>
+    <p className="w-304">
+      Bli med på Knowits julekalender! Hver dag før jul åpnes en ny luke med
+      en oppgave du må løse. Vi trekker en heldig vinner som får en valgfri
+      mobil eller nettbrett!
+    </p>
 
-      <div className="space-x-8">
-        <span className="font-bold">Er du Nordpolens flittigste alv?</span>
-        <RegisterButton />
-      </div>
-    </PageContent>
-  )
-}
+    <div className="space-x-8">
+      <span className="font-bold">Er du Nordpolens flittigste alv?</span>
+      <RegisterButton />
+    </div>
+  </PageContent>
+)
 
 export default Countdown
