6b08aa7727c2247bf04b7e276012f0a28073734d
Hugo Wallenburg <hugo@hugow.no>
2023-11-29 14:28:17 +0100

    Improves vite environment typing

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,7 +1,12 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_BACKEND_HOST: string
+  VITE_ENABLE_LIGHT_MODE: string
+}
+
 interface ImportMeta {
-  env: {
-    VITE_BACKEND_HOST: string
-    VITE_ENABLE_LIGHT_MODE: string
-  }
+  readonly env: ImportMetaEnv
 }
