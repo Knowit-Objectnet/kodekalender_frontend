ac74b9ca3ebf2fdf7a349dda82f99874caa47b09
Gaute Rauboti <gaute.rauboti@knowit.no>
2023-11-30 22:55:55 +0100

    Reverted changes to file

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -2,20 +2,19 @@ import DoorBorder from "../components/Door/DoorBorder"
 import { FCWithChildren } from "../../types/utils_types"
 import PageContent from "../components/PageContent"
 
+
 type PageProps = {
   className?: string
   wrapperClassName?: string
 }
 
-const Page: FCWithChildren<PageProps> = ({
-  className,
-  wrapperClassName,
-  children
-}) => (
+const Page: FCWithChildren<PageProps> = ({ className, wrapperClassName, children }) => (
   <PageContent className={wrapperClassName}>
     {/* <DoorBorder /> */}
 
-    <div className={className}>{children}</div>
+    <div className={className}>
+      {children}
+    </div>
   </PageContent>
 )
 
