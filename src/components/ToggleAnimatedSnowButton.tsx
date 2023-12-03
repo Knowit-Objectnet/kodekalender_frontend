abea1996341d1788a019848a33996214ae564041
Hugo Wallenburg <hugo@hugow.no>
2023-12-03 16:13:19 +0100

    Allows overriding prefers-reduced-motion, toggles on change

--- src/components/ToggleAnimatedSnowButton.tsx
+++ src/components/ToggleAnimatedSnowButton.tsx
@@ -1,13 +1,11 @@
-import { forwardRef, useCallback, useContext, useId } from "react"
+import { forwardRef, useCallback, useContext } from "react"
 
 import { OptionsContext } from "../OptionsContext"
-import usePrefersReducedMotion from "../hooks/mediaQueries/usePrefersReducedMotion"
 
 import Button, { ButtonProps } from "./Button"
 
 
 const ToggleAnimatedSnowButton = forwardRef<HTMLButtonElement, ButtonProps>(({ onClick, ...buttonProps }, ref) => {
-  const prefersReducedMotion = usePrefersReducedMotion()
   const { showSnow, setShowSnow } = useContext(OptionsContext)
 
   const toggleSnow = useCallback(() => {
@@ -15,35 +13,22 @@ const ToggleAnimatedSnowButton = forwardRef<HTMLButtonElement, ButtonProps>(({ o
   }, [setShowSnow])
 
   const [content, ariaContent] =
-    (showSnow && !prefersReducedMotion)
+    showSnow
       ? ["Skru av snø", "Skru av animert snø"]
       : ["Skru på snø", "Skru på animert snø"]
 
-  const description =
-    prefersReducedMotion
-      ? "Animasjoner er satt til redusert i operativsystemet ditt. Skru av dette for å kunne se nydelig animert snø ☃️."
-      : ""
-
-  const id = useId()
-
   return (
-    <>
-      <Button
-        ref={ref}
-        icon="snow"
+    <Button
+      ref={ref}
+      icon="snow"
 
-        onClick={(e) => { onClick?.(e); toggleSnow() }}
-        disabled={prefersReducedMotion}
+      onClick={(e) => { onClick?.(e); toggleSnow() }}
 
-        content={content}
-        title={description}
-        aria-label={ariaContent}
-        aria-describedby={description ? id : undefined}
+      content={content}
+      aria-label={ariaContent}
 
-        {...buttonProps}
-      />
-      {description && <span id={id} className="sr-only">{description}</span>}
-    </>
+      {...buttonProps}
+    />
   )
 })
 
