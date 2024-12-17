4e5cdd4acde5ea8661d8a34a4aee3d70de7570e1
Hugo Wallenburg <hugo@hugow.no>
2024-12-17 16:47:58 +0100

    Updates career page with link to Bergen, Trondheim

--- src/pages/Career.tsx
+++ src/pages/Career.tsx
@@ -1,24 +1,50 @@
 import { LinkButton } from "../components/LinkButton"
 
+import Divider from "../components/Divider"
 import BasicPage from "./BasicPage"
 
 const Career = () => (
-  <BasicPage title="Jobbe i Knowit?">
-    <p>
-      Dersom du ønsker å vite mer om Knowit må du gjerne ta kontakt med en av våre alver for en
-      uformell prat!
-    </p>
+  <BasicPage title="Jobbe i Knowit?" containerClassName="gap-24">
+    <div className="grid gap-12">
+      <p>
+        Vi er alltid på jakt etter dyktige alver i våre rekker! Kanskje kan du hjelpe oss med neste års julekalender?
+      </p>
 
-    <div className="flex flex-wrap justify-center gap-8">
-      <LinkButton
-        className="underline"
-        to="https://careernorway.knowit.no/jobs/1480266-systemutvikler-backend-mobil-og-eller-frontend"
-        title="Søk på stilling som Systemutvikler Backend, Mobil og/eller Frontend!"
-        content="Jobb i Knowit"
-        icon="link"
-        external
-      />
-      <LinkButton to="mailto:julenissen@knowit.no" content="Kontakt alvene" icon="mail" />
+      <div className="flex flex-wrap justify-center gap-8">
+        <LinkButton
+          to="https://careernorway.knowit.no/jobs/1480266-systemutvikler-backend-mobil-og-eller-frontend"
+          title="Søk på stilling i Oslo: Systemutvikler Backend, Mobil og/eller Frontend!"
+          content="Jobb i Oslo"
+          icon="link"
+          external
+        />
+        <LinkButton
+          to="https://careernorway.knowit.no/jobs/1478548-du-er-utvikleren-som-utvikler-oss"
+          title="Søk på stilling i Bergen: Du er utvikleren som utvikler oss!"
+          content="Jobb i Bergen"
+          icon="link"
+          external
+        />
+        <LinkButton
+          to="https://www.knowit.no/jobb-hos-oss/ledige-stillinger/vi-soker-utvikler-i-trondheim/"
+          title="Søk på stilling i Trondheim: Bli med på Knowits eventyr i Trondheim!"
+          content="Jobb i Trondheim"
+          icon="link"
+          external
+        />
+      </div>
+    </div>
+
+    <Divider />
+
+    <div className="grid gap-12">
+      <p>
+        Om du ønsker å vite mer om å jobbe i Knowit kan du ta kontakt med julenissen for en prat.
+      </p>
+
+      <div className="flex flex-wrap justify-center gap-8">
+        <LinkButton to="mailto:julenissen@knowit.no" content="julenissen@knowit.no" icon="mail" />
+      </div>
     </div>
   </BasicPage>
 )
